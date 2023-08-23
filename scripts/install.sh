bd0b92bc16bdfdbf194c251595d7fd6f500d4a5f
Kiran Thyagaraja <18704819+kirankt@users.noreply.github.com>
2023-08-22 21:15:52 -0500

    Bug fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,16 +6,16 @@ K3D=./k3d
 ARGS="--registry-use k3d-airgap.localhost:5000 --registry-config registry.yaml"
 
 if [[ "###ZARF_VAR_ENABLE_GPUS###" == "true" ]]; then
-	ARGS += " --gpus=###ZARF_VAR_GPU_COUNT### --image=###ZARF_VAR_CUSTOM_K3D_IMAGE###"
+	ARGS+=" --gpus=###ZARF_VAR_GPU_COUNT### --image=###ZARF_VAR_K3S_CUDA_IMAGE###"
 fi
 if [[ "###ZARF_VAR_ENABLE_SERVICE_LB###" == "false" ]]; then
-	ARGS += " --no-lb"
+	ARGS+=" --no-lb"
 fi
 if [[ "###ZARF_VAR_ENABLE_TRAEFIK###" == "false" ]]; then
-	ARGS += " --disable=traefik@server:*"
+	ARGS+=" --disable=traefik@server:*"
 fi
 if [[ "###ZARF_VAR_ENABLE_METRICS_SERVER###" == "false" ]]; then
-	ARGS += " --disable=metrics-server@server:*"
+	ARGS+=" --disable=metrics-server@server:*"
 fi
 
 # Check if the cluster already exists
